3b572f24d51668cdb36cddafec5263bee7cf0381
Kiara Singh <st10215222@vcconnect.edu.za>
2024-05-27 21:33:06 +0200

    Initial Commit

---
+++ BasicTesting/BasicTesting/Maths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTesting
+{
+    public class Maths
+    {
+
+        //method -- returning -- string (non - static = return statement)
+        public string printMsg (string msg)
+        {
+            Console.WriteLine("Test: " + msg);
+            return msg;
+        }
+
+        //method 2 -- returning -- 2 ints
+        public int addVals(int a, int b)
+        { 
+            int result = a + b;
+            Console.WriteLine("Values added: " + result);
+            return result;
+        }
+
+    }
+}
---
+++ BasicTesting/UnitTestProject1/MathsTest.cs
@@ -0,0 +1,37 @@
+﻿using BasicTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MathsTest
+    {
+        [TestMethod]
+        public void TestPrint()
+        {
+            //arrange 
+            string expected = "John Smith";
+
+            //actual -- using basic testing under light bulb
+            Maths math = new Maths();
+            string actual = math.printMsg("John Smith");
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+            
+        public void TestaddVals()
+        {
+            int expected = 500;
+
+            Maths math  = new Maths();
+            int actual = math.addVals(400, 100);
+            
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
